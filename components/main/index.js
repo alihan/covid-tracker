002741cf1e1780fac585819eaad18761eb71a445
alihany <alihanyiilmaz@gmail.com>
2020-10-15 21:59:17 +0300

    get some data to display in card

--- components/main/index.js
+++ components/main/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchGlobalData } from '../../pages/api/country';
+import Card from '../card'
 
 import style from './style.module.scss'
 
 function Main({ children }) {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+  const fetchAPI = async() => {
+    const data = await fetchGlobalData();
+    setData(data);
+  };
+  fetchAPI();
+  }, []);
+
   return (
-    <main>
-      <h2 className={style.title}>this is h2</h2>
-      <section>{children}</section>
+    <main className={style.main}>
+      <h2 className={style.title}>Coronavirus COVID-19 Global Cases</h2>
+      <section>
+        <Card data={data} />
+      </section>
     </main>
   )
 }

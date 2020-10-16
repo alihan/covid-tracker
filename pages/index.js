ac2d37de963c009246776f0bb6c61deb84ac83b5
alihany <alihanyiilmaz@gmail.com>
2020-10-16 23:54:10 +0300

    fix pages

--- pages/index.js
+++ pages/index.js
@@ -11,57 +11,9 @@ import Layout from '../components/layout'
 
 
 const Profile = () => {
-  const [type, setType] = useState('cases');
-  const [country, setCountry] = useState('');
-  const [dailyData, setDailyData] = useState();
-
-  const { data, error } = useSWR(
-    `https://disease.sh/v3/covid-19/countries
-    `,
-    fetcher
-  );
-
-  useEffect(() => {
-    const fetchAPI = async (country) => {
-      const data = await fetchData(country);
-      setDailyData(data);
-    };
-    fetchAPI();
-  }, []);
-
-  const handleCountryChange = async (country) => {
-    const countryData = await fetchData(country);
-    setDailyData(countryData);
-    setCountry(country);
-  };
-
-  console.log(dailyData);
-  console.log(type);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  
   return (
-    <Layout>
-      <div>
-        <CountryPicker
-          country={country}
-          handleCountryChange={handleCountryChange}
-        />
-        <div className="button-container">
-          <button onClick={() => setType('cases')}>Cases</button>
-          <button onClick={() => setType('recovered')}>Recovered</button>
-          <button onClick={() => setType('deaths')}>Deaths</button>
-        </div>
-        {type === 'cases' && country && (
-          <LineExample data={dailyData.cases} country={country} />
-        )}
-        {type === 'recovered' && country && (
-          <LineRecovered data={dailyData.recovered} country={country} />
-        )}
-        {type === 'deaths' && country && (
-          <LineDeaths data={dailyData.deaths} country={country} />
-        )}
-        </div>
-    </Layout>
+    <Layout />
   );
 };
 export default Profile;

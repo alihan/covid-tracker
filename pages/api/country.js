8e57b0449911ff53aad4fbee89229080a5e68824
alihany <alihanyiilmaz@gmail.com>
2020-10-18 21:25:16 +0300

    numberformat and dataformat added

--- pages/api/country.js
+++ pages/api/country.js
@@ -1,52 +1,99 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const url = 'https://covid19.mathdro.id/api';
+const url = 'https://covid19.mathdro.id/api'
 
 export const fetchData = async (country) => {
-  let changeableUrl = url;
+  let changeableUrl = url
 
   if (country) {
-    changeableUrl = `https://disease.sh/v3/covid-19/historical/${country}?lastdays=15
-    `;
-  }
-  try {
-    const {
-      data: {
-        timeline: { cases, deaths, recovered },
-      },
-    } = await axios.get(changeableUrl);
+    if (country === 'Global') {
+      changeableUrl = `https://disease.sh/v3/covid-19/historical/all?lastdays=15`
+      try {
+        const {
+          data: { cases, deaths, recovered }
+        } = await axios.get(changeableUrl)
 
-    return { cases, deaths, recovered };
-  } catch (error) {
-    return error;
+        return { cases, deaths, recovered }
+      } catch (error) {
+        return error
+      }
+    } else {
+      changeableUrl = `https://disease.sh/v3/covid-19/historical/${country}?lastdays=15
+    `
+      try {
+        const {
+          data: {
+            timeline: { cases, deaths, recovered }
+          }
+        } = await axios.get(changeableUrl)
+
+        return { cases, deaths, recovered }
+      } catch (error) {
+        return error
+      }
+    }
   }
-};
+}
 
 export const fetchGlobalData = async () => {
   try {
-    const { data : {
-      cases, deaths, recovered, casesPerOneMillion, deathsPerOneMillion, recoveredPerOneMillion
-    } } = await axios.get(
-      'https://disease.sh/v3/covid-19/all'
-    );
+    const {
+      data: {
+        cases,
+        deaths,
+        recovered,
+        casesPerOneMillion,
+        deathsPerOneMillion,
+        recoveredPerOneMillion
+      }
+    } = await axios.get('https://disease.sh/v3/covid-19/all')
 
-    return {cases, deaths, recovered, casesPerOneMillion, deathsPerOneMillion, recoveredPerOneMillion};
+    return {
+      cases,
+      deaths,
+      recovered,
+      casesPerOneMillion,
+      deathsPerOneMillion,
+      recoveredPerOneMillion
+    }
   } catch (error) {
-    return error;
+    return error
   }
-};
+}
 
 export const fetchCountryData = async (country) => {
-  try {
-    const { data : {
-      cases, deaths, recovered, casesPerOneMillion, deathsPerOneMillion, recoveredPerOneMillion
-    } } = await axios.get(
-      `https://disease.sh/v3/covid-19/countries/${country}?strict=true
-      `
-    );
+  let changeableUrl = url
 
-    return {cases, deaths, recovered, casesPerOneMillion, deathsPerOneMillion, recoveredPerOneMillion};
-  } catch (error) {
-    return error;
+  if (country) {
+    if (country === 'Global') {
+      changeableUrl = `https://disease.sh/v3/covid-19/all?yesterday=true
+      `
+    } else
+      changeableUrl = `https://disease.sh/v3/covid-19/countries/${country}?strict=true
+      `
   }
-};
+
+  try {
+    const {
+      data: {
+        cases,
+        deaths,
+        recovered,
+        casesPerOneMillion,
+        deathsPerOneMillion,
+        recoveredPerOneMillion
+      }
+    } = await axios.get(changeableUrl)
+
+    return {
+      cases,
+      deaths,
+      recovered,
+      casesPerOneMillion,
+      deathsPerOneMillion,
+      recoveredPerOneMillion
+    }
+  } catch (error) {
+    return error
+  }
+}

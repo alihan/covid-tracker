2c1b1b7c46244446d205dea5232253f658e174bd
alihany <alihanyiilmaz@gmail.com>
2020-10-20 23:49:28 +0300

    added loading component

--- components/loading/index.js
+++ components/loading/index.js
@@ -5,23 +5,91 @@ function Loading({ size = 30, stroke = 2 }) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 38 38"
+      viewBox="0 0 58 58"
       xmlns="http://www.w3.org/2000/svg"
-      stroke="rgba(29, 161, 242, 1)"
     >
-      <g fill="none" fillRule="evenodd">
-        <g transform="translate(1 1)" strokeWidth={stroke}>
-          <circle strokeOpacity=".3" cx="18" cy="18" r="18" />
-          <path d="M36 18c0-9.94-8.06-18-18-18">
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="0 18 18"
-              to="360 18 18"
-              dur="1s"
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(2 1)" stroke="#FFF" stroke-width="1.5">
+          <circle cx="42.601" cy="11.462" r="5" fill-opacity="1" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="1;0;0;0;0;0;0;0"
+              calcMode="linear"
               repeatCount="indefinite"
             />
-          </path>
+          </circle>
+          <circle cx="49.063" cy="27.063" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;1;0;0;0;0;0;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="42.601" cy="42.663" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;1;0;0;0;0;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="27" cy="49.125" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;0;1;0;0;0;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="11.399" cy="42.663" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;0;0;1;0;0;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="4.938" cy="27.063" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;0;0;0;1;0;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="11.399" cy="11.462" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;0;0;0;0;1;0"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
+          <circle cx="27" cy="5" r="5" fill-opacity="0" fill="#fff">
+            <animate
+              attributeName="fill-opacity"
+              begin="0s"
+              dur="1.3s"
+              values="0;0;0;0;0;0;0;1"
+              calcMode="linear"
+              repeatCount="indefinite"
+            />
+          </circle>
         </g>
       </g>
     </svg>

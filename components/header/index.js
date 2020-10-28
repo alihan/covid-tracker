8b901ec9e91280b5728802c2d20754e90bf6d961
alihany <alihanyiilmaz@gmail.com>
2020-10-28 18:18:33 +0300

    changed images to icons & added link routers to header

--- components/header/index.js
+++ components/header/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 import style from './style.module.scss'
+import * as Icon from '../icons'
 
 function Header() {
   return (
     <nav className={style.header}>
-      <div className={style.titleContainer}>
-        <h1 className={style.title}>COVID-19 Tracker</h1>
-        <img
-          src="https://www.flaticon.com/svg/static/icons/svg/2860/2860597.svg"
-          alt="covid"
-          className={style.icon}
-        />
-      </div>
+      <Link href="/">
+        <div className={style.titleContainer}>
+          <h1 className={style.title}>COVID-19 Tracker</h1>
+
+          <Icon.Covid />
+        </div>
+      </Link>
+
+      <Link href="/how-to-protect">
+        <a className={style.pageHref}>How To Protect</a>
+      </Link>
     </nav>
   )
 }

10eb3bc0a2ea0b172ce1ee5f82234843460063a5
alihany <alihanyiilmaz@gmail.com>
2020-10-28 18:16:27 +0300

    fixed layout for multiple pages

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,12 +4,16 @@ import style from './style.module.scss'
 
 import Header from '../header'
 import Main from '../main'
+import Footer from '../footer'
 
-function Layout() {
+function Layout({ home, howToProtect, children }) {
   return (
     <div className={style.layout}>
       <Header />
-      <Main />
+      <Main home={home} howToProtect={howToProtect}>
+        {children}
+      </Main>
+      <Footer />
     </div>
   )
 }

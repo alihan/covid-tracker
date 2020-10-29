f761c020a6d464094aa71d208b50ff5a6512682d
alihany <alihanyiilmaz@gmail.com>
2020-10-29 17:57:59 +0300

    added title description protect page

--- pages/how-to-protect.js
+++ pages/how-to-protect.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NextSeo } from 'next-seo'
 
 import Layout from '../components/layout'
 
+const url = 'http://covid-tracker-black.vercel.app/how-to-protect'
+const title = 'How to Protect Yourself & Others'
+const description = 'See how can you protect from COVID-19'
+
 const HowToProtect = () => {
-  return <Layout howToProtect />
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+          description
+        }}
+      />
+      <Layout howToProtect />
+    </>
+  )
 }
 export default HowToProtect

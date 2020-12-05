639ad01f9c20f006a751e085c1f751f4b02dd49a
alihany <alihanyiilmaz@gmail.com>
2020-12-05 14:05:16 +0300

    Create card-item component and refactor card component

--- components/card/index.js
+++ components/card/index.js
@@ -1,44 +1,36 @@
 import React from 'react'
-import cn from 'classnames'
 import style from './style.module.scss'
 import numberFormat from '../../utils/numberFormat'
+import CardItem from '../card-item'
 
-function Card({ data: { cases, deaths, recovered, population }, country }) {
+const Card = ({ data: { cases, deaths, recovered, population }, country }) => {
   const casePercantage = ((cases * 100) / population).toFixed(2)
   const deathPercantage = ((deaths * 100) / cases).toFixed(2)
   const recoveredPercantage = ((recovered * 100) / cases).toFixed(2)
 
   return (
     <div className={style.cardContainer}>
-      <div className={style.card}>
-        <h2 className={cn(style.title, style.cases)}>{numberFormat(cases)}</h2>
-        <p className={style.description}>Cases</p>
-        <h2 className={style.info}>
-          The ratio of the number of cases to the population of
-          <span className={style.country}> {country}</span> is
-          <span className={style.highlight}> %{casePercantage}</span>
-        </h2>
-      </div>
-      <div className={style.card}>
-        <h2 className={cn(style.title, style.deaths)}>
-          {numberFormat(deaths)}
-        </h2>
-        <p className={style.description}>Deaths</p>
-        <h2 className={style.info}>
-          The ratio of the number of deaths to the number of cases
-          <span className={style.highlight}> %{deathPercantage}</span>
-        </h2>
-      </div>
-      <div className={style.card}>
-        <h2 className={cn(style.title, style.recovered)}>
-          {numberFormat(recovered)}
-        </h2>
-        <p className={style.description}>Recovered</p>
-        <h2 className={style.info}>
-          The ratio of the number of deaths to the number of cases %
-          <span className={style.highlight}> {recoveredPercantage}</span>
-        </h2>
-      </div>
+      <CardItem
+        title={numberFormat(cases)}
+        description={'Cases'}
+        info={'The ratio of the number of cases to the population of'}
+        country={country}
+        percantage={casePercantage}
+      />
+      <CardItem
+        title={numberFormat(deaths)}
+        description={'Deaths'}
+        info={'The ratio of the number of deaths to the number of cases'}
+        country={country}
+        percantage={deathPercantage}
+      />
+      <CardItem
+        title={numberFormat(recovered)}
+        description={'Recovered'}
+        info={'The ratio of the number of recoveries to the number of cases'}
+        country={country}
+        percantage={recoveredPercantage}
+      />
     </div>
   )
 }
